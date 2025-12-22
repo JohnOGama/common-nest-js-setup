4d575931a8f2817a09ecfc38629b2bba39562209
JohnOgama <jogama696@gmail.com>
2025-12-22 18:50:57 +0800

    refactor(config): update database credentials to use NEON_DB_STRING

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,12 +8,7 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'paymentDb',
-    ssl: false,
+    url: process.env.NEON_DB_STRING ?? '',
   },
   verbose: true,
   strict: true,
